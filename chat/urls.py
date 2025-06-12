34a3bfc45554b16cfa1b36a993bf125677a250c2
reginaldo-castro <reginaldo.rc.castro@gmail.com>
2025-06-11 21:12:17 -0400

    Configuração do django rest framework

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from . import views
+from .views import RoomListView, MessageListView, MessageCreateView, index, room
+from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
+from chat.schema import schema
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
+    path('', index, name='index'),
+    path('<str:room_name>/', room, name='room'),
+    path('api/rooms', RoomListView.as_view(), name='api_rooms'),
+    path('api/rooms/<int:room_id>/messages/', MessageListView.as_view(), name='api_room_messages'),
+    path('api/messages/new/', MessageCreateView.as_view(), name='api_create_message'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView
+from .models import Message, Room
+from .serializers import RoomSerializer, MessageSerializer
 
 def index(request):
     return render(request, 'chat/index.html')
@@ -7,3 +10,17 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
     })
+    
+class RoomListView(ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+class MessageListView(ListAPIView):
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        room_id = self.kwargs['room_id']
+        return Message.objects.filter(room__id=room_id)
+
+class MessageCreateView(CreateAPIView):
+    serializer_class = MessageSerializer
